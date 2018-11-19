e98a65ae6813814e01887d462d7c113737aabca4
Aleksandr Bobyr <alex.tachcard911@gmail.com>
2018-11-19 12:14:12 +0200

    add prod server

--- src/api/common.js
+++ src/api/common.js
@@ -2,5 +2,6 @@
 import axios from 'axios'
 
 export const HTTP = axios.create({
-  baseURL: 'http://localhost:8000/api/'
+  // baseURL: 'http://localhost:8000/api/'
+  baseURL: 'https://insurance-the-api-of-risks.herokuapp.com/api/'
 })
