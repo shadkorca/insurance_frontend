acefe1a12b08ace3e0daf149df4df942a725d91c
Aleksandr Bobyr <alex.tachcard911@gmail.com>
2018-12-04 12:36:38 +0200

    prod server

--- src/api/common.js
+++ src/api/common.js
@@ -2,6 +2,5 @@
 import axios from 'axios'
 
 export const HTTP = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/'
-  // baseURL: 'https://insurance-the-api-of-risks.herokuapp.com/api/'
+  baseURL: 'https://insurance-the-api-of-risks.herokuapp.com/api/'
 })
